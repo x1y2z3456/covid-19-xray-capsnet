6859b3d1b65b54d5e28bd281faba45845e9461f1
Nick Zhuang <andy345694@gmail.com>
2022-12-03 10:49:44 +0800

    [modify] seperate data to train and test

--- resize_images.py
+++ resize_images.py
@@ -3,7 +3,7 @@
 import cv2
 from os import listdir
 from os.path import isfile, join
-
+SHAPE = 96
 
 
 def getImagesFromDir(directory):
@@ -14,7 +14,7 @@ def resizeImages(files, directory):
     for f in files:
         print(f)
         image = cv2.imread(join(directory, f))
-        img = cv2.resize(image, (128, 128), interpolation=cv2.INTER_AREA)
+        img = cv2.resize(image, (SHAPE, SHAPE), interpolation=cv2.INTER_AREA)
         cv2.imwrite(join(directory, f), img)
 
 # files = getImagesFromDir('/home/user/Efficient-CapsNet/covid-chestxray-dataset/images_ok/bacteria')
@@ -22,8 +22,20 @@ def resizeImages(files, directory):
 
 # bacteria_files = getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/bacteria')
 # print(bacteria_files)
-resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/bacteria'))
-resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/healthy'))
-resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/viral/covid-19'))
-resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/viral/other'))
+
+# resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/bacteria'))
+# resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/healthy'))
+# resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/viral/covid-19'))
+# resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/images_ok/viral/other'))
+
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/train/bacteria'))
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/train/healthy'))
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/train/viral_covid-19'))
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/train/viral_other'))
+
+
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/test/bacteria'))
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/test/healthy'))
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/test/viral_covid-19'))
+resizeImages(*getImagesFromDir('/root/Efficient-CapsNet/covid-chestxray-dataset/test/viral_other'))
 
